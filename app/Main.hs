8f29fb87daddc828216de2a52a6984c1233c2aa0
erikbackman <erikb.github@gmail.com>
2019-12-15 01:57:16 +0100

    Add Day2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
+import Data.Maybe (fromMaybe)
+import System.Environment (getArgs)
+
 import qualified Day1 as Day1
+import qualified Day2 as Day2
+
+type Solution = (String -> String)
+
+solutions :: [(String, Solution)]
+solutions =
+  [ ("day1", Day1.solve)
+  , ("day2", Day2.solve)
+  ]
+
+getSolution :: String -> Solution
+getSolution day = fromMaybe (const $ "no solution for: " <> day) (lookup day solutions)
 
 main :: IO ()
-main = interact Day1.solve
+main = do
+  day <- head <$> getArgs
+  inp <- readFile $ "inputs/" <> day
+  print $ getSolution day $ inp
---
+++ src/Day2.hs
@@ -0,0 +1,57 @@
+module Day2 where
+
+import qualified Data.Vector as V
+import Data.Vector (Vector, (//))
+import Control.Monad.State
+import Control.Lens
+import Data.List.Split (splitOn)
+
+type Memory = Vector Int
+type Instructions = [Int]
+type Program = State Memory Int
+
+solve :: String -> String
+solve = show . part1 . parseInstructions
+
+part1 :: Instructions -> Int
+part1 = runProgram part1Program
+
+parseInstructions :: String -> Instructions
+parseInstructions = fmap read . splitOn "," 
+
+allocateMem :: Instructions -> Memory
+allocateMem = V.fromList
+
+restore :: Memory -> Memory
+restore memory = memory // [(1, 12), (2, 2)]
+
+runProgram :: Program -> Instructions -> Int 
+runProgram program instructions = evalState program mem
+  where mem = restore $ allocateMem instructions
+
+writeTo :: Int -> Int -> Program 
+writeTo i v = modify (\s -> s & ix i .~ v) >> readFrom 0
+
+readFrom :: Int -> Program 
+readFrom i = gets (\s -> s V.! i)
+
+readFromPosAt :: Int -> Program
+readFromPosAt = readFrom >=> readFrom
+
+part1Program :: Program
+part1Program = runOpCode 0
+  where
+    runOpCode pos = do
+      opc <- readFrom pos
+      case opc of
+        1  -> runBinOp (+)
+        2  -> runBinOp (*)
+        99 -> readFrom 0
+        _  -> error $ "unexpected optcode at pos: " <> show pos
+        where
+          runBinOp binOp = do
+              val1 <- readFromPosAt (pos+1)
+              val2 <- readFromPosAt (pos+2)
+              dest <- readFrom (pos+3)
+              void $  writeTo dest (val1 `binOp` val2)
+              runOpCode (pos+4)
